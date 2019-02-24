a1cb18b6dabbe4a7979dc6b32bcba36b0c37f10d
Sakti Dwi Cahyono <sakti@proofn.com>
2019-02-25 00:44:24 +0700

    drone: Add workspace config

--- .drone.sh
+++ .drone.sh
@@ -2,20 +2,18 @@
 set -e
 set -x
 
-# disable CGO for cross-compiling
-export CGO_ENABLED=0
+export CGO_ENABLED=1
 
 # compile for all architectures
 GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.Version=${DRONE_TAG##v}" -o release/linux/amd64/prviewer   github.com/sakti/prviewer
-GOOS=linux   GOARCH=arm64 go build -ldflags "-X main.Version=${DRONE_TAG##v}" -o release/linux/arm64/prviewer   github.com/sakti/prviewer
-GOOS=linux   GOARCH=arm   go build -ldflags "-X main.Version=${DRONE_TAG##v}" -o release/linux/arm/prviewer     github.com/sakti/prviewer
+
+export CGO_ENABLED=0
+
 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Version=${DRONE_TAG##v}" -o release/windows/amd64/prviewer.exe github.com/sakti/prviewer
 GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.Version=${DRONE_TAG##v}" -o release/darwin/amd64/prviewer  github.com/sakti/prviewer
 
 # tar binary files prior to upload
 tar -cvzf release/prviewer_linux_amd64.tar.gz   -C release/linux/amd64   prviewer
-tar -cvzf release/prviewer_linux_arm64.tar.gz   -C release/linux/arm64   prviewer
-tar -cvzf release/prviewer_linux_arm.tar.gz     -C release/linux/arm     prviewer
 tar -cvzf release/prviewer_windows_amd64.tar.gz -C release/windows/amd64 prviewer.exe
 tar -cvzf release/prviewer_darwin_amd64.tar.gz  -C release/darwin/amd64  prviewer
 
